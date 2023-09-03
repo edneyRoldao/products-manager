b8d40e5bbfcb49cdd45013ec245dc36abeb78484
edneyRoldao <edneyroldao@gmail.com>
2023-09-03 20:09:04 -0300

    product template has been added

--- products-manager-frontend/src/app/app-routing.module.ts
+++ products-manager-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductContainerComponent } from './components/product/product-container/product-container.component';
+import { ProductFormComponent } from './components/product/product-form/product-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'products',
+  children: [
+    { path: '', component: ProductContainerComponent },
+    { path: 'form', component: ProductFormComponent }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- products-manager-frontend/src/app/app.module.ts
+++ products-manager-frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductContainerComponent } from './components/product/product-container/product-container.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { ProductItemComponent } from './components/product/product-item/product-item.component';
+import { ProductFormComponent } from './components/product/product-form/product-form.component';
+import { AdminContainerComponent } from './components/admin/admin-container/admin-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductContainerComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductFormComponent,
+    AdminContainerComponent,
   ],
   imports: [
     BrowserModule,
